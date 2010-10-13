7266057d01a9101f01ed544508a1dcdffa7abb59
Felipe Coury <felipe.coury@gmail.com>
2010-10-13 14:44:30 -0300

    Added dump command to help message.

--- lib/stackfu/commands/help_command.rb
+++ lib/stackfu/commands/help_command.rb
@@ -19,6 +19,7 @@ module StackFu::Commands
       puts "    #{"config".foreground(:cyan)}                       configure StackFu CLI options"
       puts "    #{"list".foreground(:cyan)} [servers|script]        lists all the scripts and/or server under your account"
       puts "    #{"generate".foreground(:cyan)}                     creates a new script in current_dir/name"
+      puts "    #{"dump".foreground(:cyan)}                         dumps an existing script in current_dir/name"
       puts "    #{"publish".foreground(:cyan)}                      publishes the item on the current folder to StackFu.com"
       puts "" 
       puts "  For a complete guide on using StackFu from command line:"
